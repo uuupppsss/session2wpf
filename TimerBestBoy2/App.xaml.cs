2e5b27d104987421ed140bee755e1bf194d410c9
uuupppsss <ulya.ch.06@mail.ru>
2025-01-24 14:35:54 +1000

    Добавьте файлы проекта.

---
+++ TimerBestBoy2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TimerBestBoy2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
