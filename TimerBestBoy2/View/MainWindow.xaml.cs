4e9e23efe718e2cff4debb1bc4c47dc6b6db1a33
uuupppsss <ulya.ch.06@mail.ru>
2025-01-29 16:12:25 +1000

    типа граф но он кривой

--- TimerBestBoy2/View/MainWindow.xaml.cs
+++ TimerBestBoy2/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using GraphX.Controls;
+using GraphX.Logic.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimerBestBoy2.Model;
+
 
 namespace TimerBestBoy2.View
 {
@@ -19,6 +23,40 @@ namespace TimerBestBoy2.View
         public MainWindow()
         {
             InitializeComponent();
+            LoadGraph();
+        }
+
+        private void LoadGraph()
+        {
+            var graph = new OrgGraph();
+
+            var root = new OrgVertex("Дороги России");
+            var dept1 = new OrgVertex("Административный департамент");
+            var dept2 = new OrgVertex("Академия Уменя дороги");
+            var dept3 = new OrgVertex("Договорной отдел");
+            var dept4 = new OrgVertex("Общий отдел");
+            var dept5 = new OrgVertex("Лицензионный отдел");
+            var dept6 = new OrgVertex("Управление маркетинга");
+
+            graph.AddVertexRange(new[] { root, dept1, dept2, dept3, dept4, dept5, dept6 });
+
+            graph.AddEdge(new OrgEdge(root, dept1));
+            graph.AddEdge(new OrgEdge(root, dept2));
+            graph.AddEdge(new OrgEdge(root, dept3));
+            graph.AddEdge(new OrgEdge(root, dept4));
+            graph.AddEdge(new OrgEdge(root, dept5));
+            graph.AddEdge(new OrgEdge(root, dept6));
+
+            var logic = new GXLogicCore<OrgVertex, OrgEdge, OrgGraph>
+            {
+                Graph = graph
+            };
+
+            var area = new GraphArea<OrgVertex, OrgEdge, OrgGraph> { LogicCore = logic };
+            
+            area.GenerateGraph();
+
+            Content = area;
         }
     }
 }
